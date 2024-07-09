7c779edb5579fbf0ea908bc7fa993a544b0e1baf
Shashank singh <shashank.singh@binmile.com>
2024-07-09 12:19:17 +0530

    button

--- src/pages/moviesfile/MoivesCard.tsx
+++ src/pages/moviesfile/MoivesCard.tsx
@@ -1,4 +1,3 @@
-// MovieCard.tsx
 import React, { useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Card from '@mui/material/Card';
@@ -7,13 +6,11 @@ import CardActions from '@mui/material/CardActions';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 import { Movie } from '../../utils/interface/types';
 import { RootState } from '../../store';
 import { useNavigate } from 'react-router-dom';
 import { removeFromFavorites, addToFavorites } from '../../redux/uersAuth/authSlice';
-
-import Typography from '@mui/material/Typography';
-
 
 interface MovieCardProps {
     movie: Movie;
@@ -21,14 +18,14 @@ interface MovieCardProps {
 
 const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
     const dispatch = useDispatch();
-    const history = useNavigate();
+    const navigate = useNavigate();
     const currentUser = useSelector((state: RootState) => state.auth.currentUser);
     const [isExpanded, setIsExpanded] = useState(false);
 
     const isFavorite = useMemo(() => {
         return currentUser?.favorites?.some(favorite => favorite.imdbID === movie.imdbID);
     }, [currentUser?.favorites, movie.imdbID]);
-    
+
     const toggleExpand = () => {
         setIsExpanded(!isExpanded);
     };
@@ -42,104 +39,102 @@ const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
     };
 
     const navigateToMovieDetails = () => {
-        history(`/movie/${movie.imdbID}`);
+        navigate(`/movie/${movie.imdbID}`);
     };
 
     return (
-        <>
-    <Grid item xs={12}>
-        <Card
-            className="movie-card"
-            style={{
-                height: '100%',
-                marginTop: 16, // Increase margin for better spacing
-                backgroundColor: 'white',
-                transition: 'transform 0.2s',
-                borderRadius: 10, // Card border radius
-                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', // Subtle shadow for depth
-            }}
-            onMouseEnter={(e) => {
-            e.currentTarget.style.transform = 'translateY(-10px)';
-            }}
-            onMouseLeave={(e) => {
-            e.currentTarget.style.transform = 'translateY(0)';
-            }}
-        >
-            <CardMedia
-            component="img"
-                height="400"
-                image={movie.Poster}
-                alt={movie.Title}
-                onClick={navigateToMovieDetails}
+        <Grid item xs={12}>
+            <Card
+                className="movie-card"
                 style={{
-                    cursor: 'pointer',
-                    borderRadius: '10px 10px 0 0', 
-            }}
-            />
-            <CardContent>
-            <Typography
-                variant="h5"
-                gutterBottom
-                sx={{ fontWeight: 'bold', fontFamily: 'Inter', fontSize: '25px' }}
-            >
-                {movie.Title}
-            </Typography>
-            {isExpanded && (
-                <Typography variant="body2" sx={{ fontFamily: 'Roboto' }}>
-                {movie.Plot}
-                </Typography>
-            )}
-            <Button
-                size="small"
-                onClick={toggleExpand}
-                sx={{ fontWeight: 'bold', color: 'red', fontFamily: 'Inter', fontSize: '15px' }}
-            >
-                {isExpanded ? 'See Less' : 'See More'}
-            </Button>
-            <Typography
-                variant="body2"
-                sx={{ fontSize: '1rem', fontFamily: 'Inter' }}
-            >
-                <span style={{ fontWeight: 'bold' }}>Director:</span> {movie.Director}
-            </Typography>
-            <Typography
-                variant="body2"
-                sx={{ fontSize: '1rem', fontFamily: 'Inter' }}
-            >
-                <span style={{ fontWeight: 'bold' }}>Rating:</span> {movie.imdbRating}/10
-            </Typography>
-            <Typography
-                variant="body2"
-                sx={{ fontSize: '1rem', fontFamily: 'Inter' }}
-            >
-                <span style={{ fontWeight: 'bold' }}>Released:</span> {movie.Released}
-            </Typography>
-            <Typography
-                variant="body2"
-                sx={{ fontSize: '1rem', fontFamily: 'Inter' }}
-            >
-                <span style={{ fontWeight: 'bold' }}>Genre:</span> {movie.Genre}
-            </Typography>
-            </CardContent>
-            <CardActions>
-            <Button
-                onClick={toggleFavorite}
-                sx={{
-                    color: 'red',
-                    fontWeight: 'bold',
-                    textTransform: 'none',
-                    '&:hover': {
-                    color: 'black',
-                    },
+                    height: '100%',
+                    marginTop: 16,
+                    backgroundColor: 'white',
+                    transition: 'transform 0.2s',
+                    borderRadius: 10,
+                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
                 }}
-                >
-                {isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}
-                </Button>
-
-            </CardActions>
-         </Card>
+                onMouseEnter={(e) => {
+                    e.currentTarget.style.transform = 'translateY(-10px)';
+                }}
+                onMouseLeave={(e) => {
+                    e.currentTarget.style.transform = 'translateY(0)';
+                }}
+            >
+                <CardMedia
+                    component="img"
+                    height="400"
+                    image={movie.Poster}
+                    alt={movie.Title}
+                    onClick={navigateToMovieDetails}
+                    style={{
+                        cursor: 'pointer',
+                        borderRadius: '10px 10px 0 0',
+                    }}
+                />
+                <CardContent>
+                    <Typography
+                        variant="h5"
+                        gutterBottom
+                        sx={{ fontWeight: 'bold', fontFamily: 'Inter', fontSize: '25px' }}
+                    >
+                        {movie.Title}
+                    </Typography>
+                    {isExpanded && (
+                        <Typography variant="body2" sx={{ fontFamily: 'Roboto' }}>
+                            {movie.Plot}
+                        </Typography>
+                    )}
+                    <Button
+                        size="small"
+                        onClick={toggleExpand}
+                        sx={{ fontWeight: 'bold', color: 'red', fontFamily: 'Inter', fontSize: '15px' }}
+                    >
+                        {isExpanded ? 'See Less' : 'See More'}
+                    </Button>
+                    <Typography
+                        variant="body2"
+                        sx={{ fontSize: '1rem', fontFamily: 'Inter' }}
+                    >
+                        <span style={{ fontWeight: 'bold' }}>Director:</span> {movie.Director}
+                    </Typography>
+                    <Typography
+                        variant="body2"
+                        sx={{ fontSize: '1rem', fontFamily: 'Inter' }}
+                    >
+                        <span style={{ fontWeight: 'bold' }}>Rating:</span> {movie.imdbRating}/10
+                    </Typography>
+                    <Typography
+                        variant="body2"
+                        sx={{ fontSize: '1rem', fontFamily: 'Inter' }}
+                    >
+                        <span style={{ fontWeight: 'bold' }}>Released:</span> {movie.Released}
+                    </Typography>
+                    <Typography
+                        variant="body2"
+                        sx={{ fontSize: '1rem', fontFamily: 'Inter' }}
+                    >
+                        <span style={{ fontWeight: 'bold' }}>Genre:</span> {movie.Genre}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button
+                        onClick={toggleFavorite}
+                        disabled={!currentUser}
+                        sx={{
+                            color: 'red',
+                            fontWeight: 'bold',
+                            textTransform: 'none',
+                            '&:hover': {
+                                color: 'black',
+                            },
+                        }}
+                    >
+                        {isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}
+                    </Button>
+                </CardActions>
+            </Card>
         </Grid>
-        </>
     );
 };
 
